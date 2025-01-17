5bd0912689d939eeb42d38700eaed8b03c9941d9
Jim Zhou <jimzhouzzy@gmail.com>
2025-01-17 21:45:39 +0800

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'data',
+    password: 'Zzycsd_189@163.com',
+    database: 'junqi_server',
 });
 
 module.exports = pool;
