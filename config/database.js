fd82703d69c2abc49238897e9282be0ac76d5d11
Tommy_Tang <tommoy_tang@Tommoy-Tangs-MacBook-Air.local>
2025-01-17 19:23:27 +0800

    Add some methods to the JunqiGame class to make the game more intriguing.

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Zzycsd_189@163.com',
-    database: 'junqi_server',
+    password: '',
+    database: 'data',
 });
 
 module.exports = pool;
