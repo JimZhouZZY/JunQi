38e925aadcb15c9aa9274e699bbbabc1ea095b7a
Jim Zhou <jimzhouzzy@gmail.com>
2025-01-27 21:23:03 +0800

    Update variables.js comment

--- sockets/variables.js
+++ sockets/variables.js
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2025 Zhiyu Zhou (jimzhouzzy@gmail.com)
+ * This file is part of Web-JunQi.
+ * Licensed under the GPLv3 License.
+ */
+
+/**
+ * sockets/variables.js
+ * 
+ * This module shares some variables that are involved with socket operations
+ * TODO: refactor needed
+ */
+
 const usernameSocketMap = {};  // A map to associate usernames with their respective socket IDs
 
 module.exports = { usernameSocketMap };
